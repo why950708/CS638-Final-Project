56d92d220984cc1b8c1b14aa1dc78c38a48b7e6d
aaamber <lxfbigsmile@gmail.com>
2017-04-24 00:16:02 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -1,10 +1,6 @@
 
 # coding: utf-8
 
-# In[ ]:
-
-
-
 # In[34]:
 from datetime import datetime 
 import tensorflow as tf
@@ -73,81 +69,51 @@ class LSTM_Model:
         self.feature_embedding = feature_embedding
     
     def _build_model(self):
-            lstm_cell = tf.contrib.rnn.BasicLSTMCell(
-                num_units = self.config.H, state_is_tuple =True)
-            
-           
-            # drop out is not included
-            with tf.variable_scope("lstm", initializer = self.initializer) as lstm_scope:
-                self.caption_out = tf.placeholder(tf.int32,[self.config.batch_size, self.config.input_len], name="caption_out")
-                self.caption_mask = tf.placeholder(tf.int32,[self.config.batch_size, self.config.input_len], name="caption_mask")
-                zero_state = lstm_cell.zero_state(
-                    batch_size=self.config.batch_size,dtype=tf.float32)
-                _, initial_state = lstm_cell(self.feature_embedding, zero_state)
-                
-                lstm_scope.reuse_variables()
-                sequence_len = tf.reduce_sum(self.caption_mask,1)
-                lstm_out, _ = tf.nn.dynamic_rnn(cell=lstm_cell,
-                                                  inputs = self.word_embedding,
-                                                  sequence_length = sequence_len,
-                                                  initial_state = initial_state,
-                                                  dtype = tf.float32,
-                                                  scope = lstm_scope)
-            lstm_out = tf.reshape(lstm_out, [-1, lstm_cell.output_size])
-            
-            with tf.variable_scope("logits"):
-                w = tf.get_variable('w', [lstm_cell.output_size, self.config.vocab_size], initializer=self.initializer)
-                b = tf.get_variable('b', [self.config.vocab_size], initializer=tf.constant_initializer(0.0))
-                # (Nt)*H ,H*v =Nt.V, bias is zero
-                logits = tf.matmul(lstm_out,w)+b
-            
-            with tf.variable_scope("loss"):
-                targets = tf.reshape(self.caption_out,[-1])
-                mask = tf.to_float(tf.reshape(self.caption_mask,[-1]))
-                loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=targets,
-                                                         logits = logits)
-                batch_loss = tf.div(tf.reduce_sum(tf.multiply(loss, mask)),
-                                    tf.reduce_sum(mask),
-                                    name="batch_loss")
-                tf.losses.add_loss(batch_loss)
-                self.total_loss = tf.losses.get_total_loss()
-                
-            with tf.name_scope("summaries"):
-                #tf.summary.scalar("loss", self.batch_loss)
-                tf.summary.scalar("loss", self.total_loss)
-                tf.summary.histogram("histogram loss", self.total_loss)
-                self.summary_op = tf.summary.merge_all()
-                
-    def _create_optimizer(self):
-        with tf.device('/cpu:0'):
-            # decay learning rate
-            num_batches = self.config.total_instances/self.config.batch_size
-            decay_steps = int(num_batches*self.config.num_epochs_per_decay)
-            learning_rate = self.config.initial_learning_rate
+        lstm_cell = tf.contrib.rnn.BasicLSTMCell(
+            num_units = self.config.H, state_is_tuple =True)
 
 
-            def _decay_fn(learning_rate, global_step):
-                return tf.train.exponential_decay(learning_rate,
-                                                 global_step,
-                                                 decay_steps = decay_steps,
-                                                 decay_rate=0.5,
-                                                 staircase=True)
+        # drop out is not included
+        with tf.variable_scope("lstm", initializer = self.initializer) as lstm_scope:
+            self.caption_out = tf.placeholder(tf.int32,[self.config.batch_size, self.config.input_len], name="caption_out")
+            self.caption_mask = tf.placeholder(tf.int32,[self.config.batch_size, self.config.input_len], name="caption_mask")
+            zero_state = lstm_cell.zero_state(
+                batch_size=self.config.batch_size,dtype=tf.float32)
+            _, initial_state = lstm_cell(self.feature_embedding, zero_state)
 
-            learning_rate_decay_fn = _decay_fn
-            self.optimizer = tf.contrib.layers.optimize_loss(loss=self.total_loss,
-                                                      global_step = self.global_step,
-                                                      learning_rate = learning_rate,
-                                                      optimizer = 'SGD',
-                                                      clip_gradients = self.config.clip_gradients,
-                                                      learning_rate_decay_fn =learning_rate_decay_fn)
+            lstm_scope.reuse_variables()
+            sequence_len = tf.reduce_sum(self.caption_mask,1)
+            lstm_out, _ = tf.nn.dynamic_rnn(cell=lstm_cell,
+                                              inputs = self.word_embedding,
+                                              sequence_length = sequence_len,
+                                              initial_state = initial_state,
+                                              dtype = tf.float32,
+                                              scope = lstm_scope)
+        lstm_out = tf.reshape(lstm_out, [-1, lstm_cell.output_size])
 
+        with tf.variable_scope("logits"):
+            w = tf.get_variable('w', [lstm_cell.output_size, self.config.vocab_size], initializer=self.initializer)
+            b = tf.get_variable('b', [self.config.vocab_size], initializer=tf.constant_initializer(0.0))
+            # (Nt)*H ,H*v =Nt.V, bias is zero
+            logits = tf.matmul(lstm_out,w)+b
 
+        with tf.variable_scope("loss"):
+            targets = tf.reshape(self.caption_out,[-1])
+            mask = tf.to_float(tf.reshape(self.caption_mask,[-1]))
+            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=targets,
+                                                     logits = logits)
+            batch_loss = tf.div(tf.reduce_sum(tf.multiply(loss, mask)),
+                                tf.reduce_sum(mask),
+                                name="batch_loss")
+            tf.losses.add_loss(batch_loss)
+            self.total_loss = tf.losses.get_total_loss()
+            tf.summary.scalar("loss", self.total_loss)
+            tf.merge_all_summaries()
 
     def build_graph(self):
         #tf.reset_default_graph()
         self._build_embedding()
         self._build_model()
-        self._create_optimizer()
 
 def train_model(model, config, data):
     
@@ -162,24 +128,52 @@ def train_model(model, config, data):
         ckpt = tf.train.get_checkpoint_state(os.path.dirname('checkpoints/checkpoint'))
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
+            
+        ################define optimizer########
+        num_batches = config.total_instances/config.batch_size
+        decay_steps = int(num_batches*config.num_epochs_per_decay)
+        learning_rate = tf.constant(config.initial_learning_rate)
 
+        learning_rate_decay_fn = None
+        def _decay_fn(learning_rate, global_step):
+            return tf.train.exponential_decay(learning_rate,
+                                             global_step,
+                                             decay_steps = decay_steps,
+                                             decay_rate=0.5,
+                                             staircase=True)
+
+        learning_rate_decay_fn = _decay_fn
+        train_op = tf.contrib.layers.optimize_loss(loss=model.total_loss,
+                                                  global_step = model.global_step,
+                                                  learning_rate = learning_rate,
+                                                  optimizer = 'SGD',
+                                                  clip_gradients = config.clip_gradients,
+                                                  learning_rate_decay_fn =learning_rate_decay_fn)
+     
+        ##################
+        
         # 100 epoch
         total_runs = int((config.total_instances/config.batch_size)*config.num_epochs)
         initial_step = model.global_step.eval()
+        
+        ### initialize summary writer
+        #tf.summary.scalar("learing_rate", learning_rate)
         writer = tf.summary.FileWriter('./graphs', sess.graph)
+        
+        
         time_now = datetime.now()
-
         for t in range(total_runs):
 
             caption_in, caption_out, mask, image_features, urls = minibatch(data,t)
             # feed data
             feed_dict = {model.image_feature: image_features, model.caption_in: caption_in, 
                         model.caption_out: caption_out, model.caption_mask: mask}
-            total_loss,_,summary = sess.run([model.total_loss, model.optimizer, model.summary_op],
+            _, total_loss = sess.run([train_op, model.total_loss],
                                            feed_dict = feed_dict)
 
-            writer.add_summary(summary, global_step=t)
-
+            writer.add_summary(total_loss, global_step=t)
+            #writer.add_summary(learning_rate, global_step=t)
+            
             # print loss infor
             if(t+1) % 20 == 0:
                 print('(Iteration %d / %d) loss: %f, and time eclipsed: %.2f minutes' % (
@@ -210,6 +204,10 @@ def train_model(model, config, data):
         embedding.tensor_name = embedding_var.name
         
         # link this tensor to its metadata file, in this case the first 500 words of vocab
+        
+        thefile = open('processed/vocab_1000.tsv','a')
+        thefile.close()
+        
         embedding.metadata_path = 'processed/vocab_1000.tsv'
 
         # saves a configuration file that TensorBoard will read during startup.
@@ -236,4 +234,5 @@ main()
                 
         
             
+        
 
